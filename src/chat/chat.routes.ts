a436ab829756768971390c6e9988e835371c5c1d
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-07-11 12:38:57 -0400

    Updated chat routes

--- src/chat/chat.routes.ts
+++ src/chat/chat.routes.ts
@@ -3,6 +3,9 @@ import ChatController from "./chat.controller";
 
 const chat = Router();
 
+chat.get('/', ChatController.allRooms);
+chat.get('/search', ChatController.searchRooms);
 chat.post('/create', ChatController.createRoom);
+chat.delete('/:roomID', ChatController.deleteRoom);
 
 export default chat;
